1a8b062b0d90e1f9d8cb21da6b3162426bae9d08
Bram Van den Abbeele <bram.vdabbeele@hotmail.com>
2019-06-20 12:49:07 +0200

    improvements
    - messages are broadcasted
    - "{user} is typing" functionality (works with multiple users at once)

--- app/index.js
+++ app/index.js
@@ -1,21 +1,41 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var sockets = []; // holds the current online (connected) sockets and their state.
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
-  socket.broadcast.emit('user joined', 'a new user connected, say hi!');
+  let status = ''; // indicates whether the user is typing or not.
+  sockets.push([socket, 'idle']);
+  socket.broadcast.emit('user joined', 'A new user connected, say hi!');
 
   socket.on("disconnect", function () {
-    socket.broadcast.emit('user left', 'a user left the chat.');
+    let index = sockets.indexOf(socket)
+
+    if (index > -1)
+      sockets.splice(index, 1); // removing the disconnected socket from the list.
+
+    socket.broadcast.emit('user left', 'A user left the chat.');
   })
 
   socket.on('chat message', function (msg, usr, time) {
-    console.log(usr);
-    io.emit('chat message', msg, usr, time);
+    status = '';
+    socket.broadcast.emit('chat message', msg, usr, time);
+  })
+
+  socket.on('typing', function (usr) {
+    if (status !== 'typing') { // doubleckeck serverside
+      socket.broadcast.emit('typing', usr);
+    }
+  })
+
+  socket.on('stoppped typing', function (usr) {
+    if (status !== '') { // doubleckeck serverside
+      socket.broadcast.emit('stopped typing', usr);
+    }
   })
 });
 
